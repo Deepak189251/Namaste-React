de662d1bdf9b1857fafacb84317497b254195872
unknown <deepakkumarbehera2002@gmail.com>
2023-04-07 18:52:51 +0530

    Episode 1

---
+++ App.js
@@ -0,0 +1,5 @@
+const parent= React.createElement("div", {id: `parent`},[
+              React.createElement("div", {id: `child1`}, [React.createElement("h1", {}, "This is h1 child1"), React.createElement("h2", {}, "This is a h2 from child1")]),
+              React.createElement("div", {id: `child2`}, [React.createElement("h1", {}, "This is h1 child2"), React.createElement("h2", {}, "This is a h2 from child2")])]);
+const root = ReactDOM.createRoot(document.querySelector("#root"));
+root.render(parent);
