bb96c51d6e76f3aa0856012a6efc18815fa15d1b
unknown <deepakkumarbehera2002@gmail.com>
2023-04-09 12:06:55 +0530

    Secondn commit

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent= React.createElement("div", {id: `parent`},[
               React.createElement("div", {id: `child1`}, [React.createElement("h1", {}, "This is h1 child1"), React.createElement("h2", {}, "This is a h2 from child1")]),
               React.createElement("div", {id: `child2`}, [React.createElement("h1", {}, "This is h1 child2"), React.createElement("h2", {}, "This is a h2 from child2")])]);
