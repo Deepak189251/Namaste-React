20871cc6f1b0f55d623e1bd756994cd5aa2bd604
Deepak <deepakkumarbehera2002@gmail.com>
2023-04-19 19:57:38 +0530

    minor message

--- App.js
+++ App.js
@@ -3,18 +3,31 @@ import ReactDOM from "react-dom/client";
 
 // Creating element in createElement
 
-const heading = React.createElement(`h1`, {id: `heading`}, `Namaste React`);
+const heading = React.createElement(`h1`, {id: `heading`}, `Namaste React CreateElement`);
 
 console.log(heading);
 
-// JSX is not HTML in js
+// Functional component "A basic js function which returns react element"
 
-const jsxHeading = <h1 id="heading">Namaste React using JSX.</h1>
+const JsxHeading = () => <h1 id="heading">Namaste React using JSX.</h1>
 
-console.log(jsxHeading)
+console.log(JsxHeading)
+
+
+
+
+// This is component composition in react " using a component inside a component ."
+
+const HeadingComponent = () => (
+    
+    <div id="container">
+        
+        <JsxHeading />
+        {heading}
+        <h1>Namaste React using Functional component.</h1>
+     </div>
+)
 const root = ReactDOM.createRoot(document.getElementById(`root`));
 
-root.render(jsxHeading);
-
-
+root.render(<HeadingComponent />);
 
