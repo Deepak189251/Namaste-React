a395a6707a5a57ff145b0b0311b65fa15b4cdd7d
D <deepakkumarbehera2002@gmail.com>
2023-05-18 08:59:46 +0530

    Removed repeted code

--- App.js
+++ App.js
@@ -1,46 +1,4 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
 
-// Creating element with React.createElement
-
-
-const container = React.createElement("div",{id:"container"},[
- React.createElement("h1",{className:"title"},"This is heading1"),
- React.createElement("h2",{className:"title"},"This is heading2"),
- React.createElement("h3",{className:"title"},"This is heading3")]);
-
-
-// Creating same element with jsx
-
-const jContainer = (
-    <>
-    {container}
-    <div id="jcontainer">
-        <h1 className="title">This is jsx heading1</h1>
-        <h2 className="title">This is jsx heading2</h2>
-        <h3 className="title">This is jsx heading3</h3>
-    </div>
-    </>
-);
-
-// Creating same with a functional component
-
-
-const FunctionalContainer = () => (
-    <>
-    {jContainer}
-     <div id="funcontainer">
-        <h1 className="title">This is functional component heading1</h1>
-        <h2 className="title">This is functional component heading2</h2>
-        <h3 className="title">This is functional component heading3</h3>
-     </div>
-    </>
-);
-
-const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(<FunctionalContainer />);
-/*
 import React from "react";
 import ReactDOM from "react-dom/client";
 
@@ -79,4 +37,4 @@ const HeadingComponent = () => (
 )
 const root = ReactDOM.createRoot(document.getElementById(`root`));
 
-root.render(<HeadingComponent />); */
+root.render(<HeadingComponent />); 
