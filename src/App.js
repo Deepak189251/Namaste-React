ed0298ddb5622ba9e913a7b1e4e0815a3d2d1f76
D <deepakkumarbehera2002@gmail.com>
2023-05-26 21:26:44 +0530

    bug fix ongoing

--- src/App.js
+++ src/App.js
@@ -3,23 +3,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-const Header = () => {
-   return(
-     <header className="header">
-       <div className="logo-container">
-         <img className="logo" src="https://images-workbench.99static.com/uNN5i13HsdTZqMFxxZ96TF1vPXY=/99designs-contests-attachments/118/118612/attachment_118612943" alt="logo" />
-        </div>
-        <div className="nav-items">
-         <ul>
-            <li>Home</li>
-            <li>About</li>
-            <li>Contact Us</li>
-            <li>Cart</li>
-         </ul>
-        </div>
-     </header>
-   )
-}
+
 
 const data = {
    "cards": [
