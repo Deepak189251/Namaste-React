86d1a294192f3d42c58d15b5b30f8442b16befef
D <deepakkumarbehera2002@gmail.com>
2023-06-09 21:53:01 +0530

    added shimmer ui

---
+++ src/components/shimmer.js
@@ -0,0 +1,24 @@
+const Shimmer = () => {
+    return(
+        <div className="shimmer_container">
+            <div className="shimmer_card">card</div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+            <div className="shimmer_card"></div>
+        </div>
+    )
+}
+
+export default Shimmer
