6f11b9185e7182a337c9d320d7b2344737e5dedb
D <deepakkumarbehera2002@gmail.com>
2023-08-10 21:06:18 +0530

    routine development

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,15 @@
 import { headerLogoUrl } from "../utils/constant"
-import { useState } from "react"
+import { useState, useContext } from "react"
 import { Link } from "react-router-dom"
 import useOnlineStatus from "../utils/useOnlineStatus"
+import { faShoppingCart } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { CartData } from "../utils/Context"
+
+
 const Header = () => {
     const [logBtn, setLogBtn] = useState("LogIn")
+    const{cart} = useContext(CartData)
     const onlineStatus = useOnlineStatus();
     return(
       <header className="flex justify-between" style={{boxShadow: "0 0px 50px -4px rgb(0 0 0 / 0.1)"}}>
@@ -16,10 +22,11 @@ const Header = () => {
              <li className="px-4 hover:text-orange-500 text-lg font-bold"><Link className="nav-link" to={"/"}>Home</Link></li>
              <li className="px-4  hover:text-orange-500 text-lg font-bold"><Link className="nav-link" to={"/about"}>About</Link></li>
              <li className="px-4  hover:text-orange-500 text-lg font-bold"><Link className="nav-link" to={"/contactus"}>Contact Us</Link></li>
-             <li className="nav-link px-4  hover:text-orange-500 text-lg font-bold">Cart</li>
              <button className="nav-btn px-4  hover:text-orange-500 text-lg font-bold" onClick={() => {
               logBtn === "LogIn" ? setLogBtn("LogOut") : setLogBtn("LogIn")
              }}>{logBtn}</button> 
+             <li className="nav-link px-4  hover:text-orange-500 text-lg font-bold"><Link to={"/cart"}>{<FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon>} Cart {`(${cart.length})`}</Link> </li>
+             
           </ul>
          </div>
       </header>
