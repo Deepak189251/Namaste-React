dcc9ac50604c0b5b1474f701ca4591c6eb9aff7b
D <deepakkumarbehera2002@gmail.com>
2023-08-13 22:34:01 +0530

    routine development

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -1,20 +1,26 @@
 import { restaurantLogoUrl } from "../utils/constant"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faCircleXmark } from "@fortawesome/free-solid-svg-icons"
-import { useContext } from "react"
+import { useContext, useState } from "react"
 import { CartData } from "../utils/Context"
 
+import { CartState } from "../utils/Context"
+
 const CartItems = ({res}) => {
-
+    const [quantity, setquantity] = useState(0)
     const {cart, setCart} = useContext(CartData)
-
+ 
+  // const{state: {cart}, dispatch} = CartState()
     
 
-    const {imageId, name, price, id} = res
+    const {imageId, name, price, id, defaultPrice} = res
 
     const removeFromCart = () =>{
         setCart(cart.filter((res) => Number(id) !== Number(res.id)))
-        
+      /*  dispatch({
+            type: "Remove",
+            payload: res
+        })*/
      }
     return(
         <div className="container flex mb-3 border-b-2 w-[750px]">
@@ -25,11 +31,11 @@ const CartItems = ({res}) => {
             <div className="details w-[300px] pl-0 mr-8 font-semibold text-sm">
                 <p>{name}</p>
             </div>
-            <div className="quantity pr-3 mr-11 text-xs">
-                <p>1</p>
+            <div className="quantity pr-2 w-[40px] mr-11 text-xs flex">
+               <div className="pr-2">1</div>
             </div>
             <div className="price mr-12 w-[50px] text-sm font-semibold">
-                <p>{price / 100}</p>
+                <p>{price ? price / 100 : defaultPrice / 100}</p>
             </div>
             <div className="delete">
                 <p><FontAwesomeIcon icon={faCircleXmark} style={{color: "red"}} onClick={removeFromCart}></FontAwesomeIcon></p>
