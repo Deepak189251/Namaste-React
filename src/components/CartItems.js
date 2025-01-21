b7d388f8bf88094f14142c1e61254e4af09f2bf7
D <deepakkumarbehera2002@gmail.com>
2025-01-21 22:32:34 +0530

    added list

--- src/components/CartItems.js
+++ src/components/CartItems.js
@@ -5,7 +5,8 @@ import { useContext, useState, useEffect } from "react"
 import { CartData } from "../utils/Context"
 import { TotalPriceData } from "../utils/PriceContext"
 import { CartState } from "../utils/Context"
-
+import { useDispatch, useSelector } from "react-redux"
+import { decreaseQty, increaseQty, removeFromCart } from "../utils/userSlice"
 
 //import { CartState } from "../utils/Context"
 
@@ -13,22 +14,25 @@ const CartItems = ({res}) => {
    // let [quantity, setquantity] = useState(1)
    // const {cart, setCart} = useContext(CartData)
    // const {totalPrice, setTotalPrice} = useContext(TotalPriceData)
-    const{state: {cart}, dispatch} = CartState()
- 
-   
-
+   // const{state: {cart}, dispatch} = CartState()
+    const cart = useSelector(store => store.user.userCart)
+   const dispatch = useDispatch()
+   //const {imageId, name, price, id, defaultPrice, qty} = res
+   const {data, id, qty} = res
     const decreaseQuantity = () => {
-        dispatch({
+       /* dispatch({
            type: "Decrement_Qty",
            payload: res
-        })
+        })*/
+        dispatch(decreaseQty(id))
      }
    
      const increaseQuantity = () => {
-        dispatch({
+        /*dispatch({
            type: "Increment_Qty",
            payload: res
-        })
+        })*/
+       dispatch(increaseQty(id))
      }
 
   //
@@ -44,41 +48,39 @@ const CartItems = ({res}) => {
     // const {state: {cart}, dispatch} = CartState()
     
 
-    const {imageId, name, price, id, defaultPrice, qty} = res
+    
 
     
 
-    const removeFromCart = () =>{
+    const removeItem = () =>{
         //setCart(cart.filter((res) => Number(id) !== Number(res.id)))
-        dispatch({
+       /* dispatch({
             type: "Remove",
             payload: res
         })
-     }
-    /* useEffect(() => {
-        setTotalPrice(totalPrice + price * quantity)
-     }, [quantity])*/
+     */
     
-     
+     dispatch(removeFromCart(id))
+    }
      
     return(
       <div className="container">
         <div className=" flex justify-center mb-3  mt-3">
             
             <div className="image w-[100px] h-[100px] mr-8">
-                <img className=" w-[100%] h-[100%] rounded-md" src={restaurantLogoUrl + imageId} rel=""/>
+                <img className=" w-[100%] h-[100%] rounded-md" src={restaurantLogoUrl + data?.imageId} rel=""/>
             </div>
             <div className="details w-[300px] pl-0 mr-8 font-semibold text-sm pt-4">
-                <p>{name}</p>
+                <p>{data?.name}</p>
             </div>
             <div className="quantity pr-2 w-[60px] mr-11 text-xs flex  border border-gray-200 h-[30px] mt-4 pl-2 pt-[3px] cursor-pointer">
               <span onClick={decreaseQuantity} className="mt-[3px]"><FontAwesomeIcon icon={faMinus} size="xs" style={{color: "green"}}></FontAwesomeIcon></span> <div className=" pl-2 pr-2 text-sm font-semibold text-green-600">{qty}</div><span onClick={increaseQuantity} className="mt-[3px]"><FontAwesomeIcon icon={faPlus} size="xs" style={{color: "green"}}></FontAwesomeIcon></span>
             </div>
             <div className="price mr-12 w-[50px] text-sm font-semibold pt-4" >
-                <p>{price ? price / 100 * qty : defaultPrice / 100 * qty}</p>
+                <p>{data?.price ? data?.price / 100 * qty : data?.defaultPrice / 100 * qty}</p>
             </div>
-            <div className="delete pt-3 cursor-pointer pt-4">
-                <p><FontAwesomeIcon icon={faCircleXmark} size="xs" style={{color: "red"}} onClick={removeFromCart}></FontAwesomeIcon></p>
+            <div className="delete cursor-pointer pt-4">
+                <p><FontAwesomeIcon icon={faCircleXmark} size="xs" style={{color: "red"}} onClick={removeItem}></FontAwesomeIcon></p>
             </div>
         </div>
         <hr className=""></hr>
