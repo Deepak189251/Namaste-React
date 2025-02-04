3d8648c9e7891d56623a172af620155d04e75067
D <deepakkumarbehera2002@gmail.com>
2025-02-04 22:32:36 +0530

    removed unused function

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -4,13 +4,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faStar } from "@fortawesome/free-solid-svg-icons"
 
 const RestruantCard = (props) => {
-  // console.log(props)
-  // console.log(restaurantLogoUrl)
+   console.log(props)
+   console.log(restaurantLogoUrl)
     const {name, cuisines, avgRating, sla, cloudinaryImageId, id, costForTwo} = props.resdata.info
    
     return (
-       <div className="restruant-card m-3 p-3 w-[230px] h-auto bg-gray-100 rounded-lg hover:bg-gray-300" ><Link className="res-details" to={`/restaurants/${id}`}>
-          <img alt="restruant_logo" className=" rounded-md" src={restaurantLogoUrl + cloudinaryImageId}/>
+       <div className="restruant-card m-3 p-3 w-[230px]  h-auto bg-gray-100 rounded-lg hover:bg-gray-300" ><Link className="res-details" to={`/restaurants/${id}`}>
+          <img alt="restruant_logo " className=" rounded-md w-[206px] h-[180px]" src={restaurantLogoUrl + cloudinaryImageId}/>
           <h4 className="font-bold py-1 text-lg">{name}</h4>
           <h5> <FontAwesomeIcon icon={faStar} style={{color: "green"}}/>{" " + avgRating}</h5>
           <h5>{cuisines.join(", ")}</h5>
