b70c45c6fde2532584bff01e9d52a2911ed45789
D <deepakkumarbehera2002@gmail.com>
2025-02-08 22:36:24 +0530

    added breakpoints for responsiveness

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,20 @@
 module.exports = {
   content: ["./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'xs': '360px',
+        'sm': '520px',
+        'md': '800px',
+        'lg': '1024px',
+        'xl': '1280px',
+        '2xl': '1536px', // Default breakpoints
+        'custom': '1600px', // Custom breakpoint
+      },
+      boxShadow:{
+        'dm': '6px 6px 8px 4px rgb(0 0 0 / 0.1), 4px 4px 6px 4px rgb(0 0 0 / 0.1);'
+      }
+    },
   },
   plugins: [],
 }
