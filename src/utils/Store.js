d52477045a8f1dd7203bb56982ca82142e336792
D <deepakkumarbehera2002@gmail.com>
2025-02-08 22:37:58 +0530

    added new slice in store

--- src/utils/Store.js
+++ src/utils/Store.js
@@ -1,12 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
-import userReducer from "./userSlice"
+import userReducer from "./userSlice";
 import  cartReducer  from "./cartSlice";
+import searchReducer from "./searchSlice";
+
 
 const store = configureStore({
 
     reducer: {
         user: userReducer,
-        cart: cartReducer
+        cart: cartReducer,
+        search: searchReducer
     }
 })
 
