c568f5b77df1fc39e51171d5fa9e73c6cc81c88d
D <deepakkumarbehera2002@gmail.com>
2025-03-19 22:34:39 +0530

    updated optimisation

--- src/components/MenuSubSection.js
+++ src/components/MenuSubSection.js
@@ -21,11 +21,11 @@ const MenuSubSection = ({response, filter, id, resname, status}) => {
       
 
         <div id="Accordian-Header" className="heading bg-white flex justify-between" onClick={() => changeShow( show)}>
-         <h5 className="menu-heading inline-block m-[10px] mr-[100px] ml-0 font-bold">{`${title} (${filter ? productList.length : itemCards.length})`}</h5>
+         <h5 className="menu-heading inline-block m-[10px] ml-0 md:text-base text-sm font-bold">{`${title} (${filter ? productList.length : itemCards.length})`}</h5>
          <button className="menu-btn border-none m-0 p-0 bg-none text-inherit cursor-pointer outline-none" >{show? <FontAwesomeIcon icon={faAngleUp} /> : <FontAwesomeIcon icon={faAngleDown}/>}</button>
         </div>
         <div id="Accordian-body" className="menu-border w-[100%] h-[13px] bg-gray-100 mb-[15px] mt-[5px]"></div>
-          {show && <div className="answer w-[650px] m-auto">  {filter ? (productList.map((res, index)=>{
+          {show && <div className="answer lg:w-[760px] md:w-[680px] sm:w-[440px] w-[250px] m-auto">  {filter ? (productList.map((res, index)=>{
         return <MenuItems key= {index} status = {status} response = {res.card.info} id={id} resname={resname}/>
       })) : (  itemCards.map((res, index)=>{
         return <MenuItems key= {index} status = {status} response = {res.card.info} id={id} resname={resname}/>
